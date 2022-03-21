3779cd25cfb3664a4c8761edcb0058549bb76d98
GeePaw Hill <geepawhill@geepawhill.org>
2022-03-21 10:25:58 -0400

    Server now times out and tests for exit.

--- making/src/test/kotlin/org/geepawhill/rw/making/NothingTest.kt
+++ making/src/test/kotlin/org/geepawhill/rw/making/NothingTest.kt
@@ -6,6 +6,8 @@ import java.io.InputStreamReader
 import java.io.PrintStream
 import java.net.ServerSocket
 import java.net.Socket
+import java.net.SocketTimeoutException
+import java.util.concurrent.atomic.AtomicBoolean
 
 class Client() {
     val socket = Socket("localhost", 3000)
@@ -21,22 +23,36 @@ class Client() {
     }
 }
 
-class Server() {
+class ContinueFlag {
+    private val keepGoing = AtomicBoolean(true)
+    val isTrue: Boolean get() = keepGoing.get()
+
+    @Synchronized
+    fun finished() {
+        keepGoing.set(false)
+    }
+}
+
+class Server(val continueFlag: ContinueFlag) {
     fun run() {
         println("Starting server thread.")
         val socket = ServerSocket(3000)
-        while (true) {
-            val accepted = socket.accept()
-            val client = Thread {
-                println("Accepted socket")
-                val input = BufferedReader(InputStreamReader(accepted.getInputStream()))
-                while (true) {
-                    val message = input.readLine()
-                    val output = PrintStream(accepted.getOutputStream(), true)
-                    output.println("You said '$message'")
+        while (continueFlag.isTrue) {
+            socket.soTimeout = 100
+            try {
+                val accepted = socket.accept()
+                val client = Thread {
+                    println("Accepted socket")
+                    val input = BufferedReader(InputStreamReader(accepted.getInputStream()))
+                    while (true) {
+                        val message = input.readLine()
+                        val output = PrintStream(accepted.getOutputStream(), true)
+                        output.println("You said '$message'")
+                    }
                 }
+                client.start()
+            } catch (ignored: SocketTimeoutException) {
             }
-            client.start()
         }
         println("Ending server thread.")
         socket.close()
@@ -57,7 +73,8 @@ class NothingTest {
 
     private fun connectOnce() {
         // Start a server
-        val server = Server()
+        val inTest = ContinueFlag()
+        val server = Server(inTest)
         val serverThread = Thread {
             server.run()
         }
@@ -66,6 +83,7 @@ class NothingTest {
         val client1 = Client()
         client1.send("Hi Mom!")
         client1.send("Hey Dad!")
+        inTest.finished()
         serverThread.join()
     }
 }
