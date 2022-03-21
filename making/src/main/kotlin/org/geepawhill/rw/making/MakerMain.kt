0b71f6097efa16344296612b97da27d2ae93bb9c
GeePaw Hill <geepawhill@geepawhill.org>
2022-03-21 10:03:38 -0400

    First take on sockets in NothingTest.

--- making/src/main/kotlin/org/geepawhill/rw/making/MakerMain.kt
+++ making/src/main/kotlin/org/geepawhill/rw/making/MakerMain.kt
@@ -1,10 +1,24 @@
 package org.geepawhill.rw.making
 
-import tornadofx.App
-import tornadofx.launch
+import tornadofx.*
+import java.net.ServerSocket
+import java.net.Socket
 
 class MakerMain : App(MakerView::class)
 
-fun main(args:Array<String>) {
-    launch<MakerMain>(args)
+fun main(args: Array<String>) {
+    // Start a server
+    val server = Thread {
+        println("Starting server thread.")
+        val socket = ServerSocket(3000)
+        socket.accept()
+        println("Ending server thread.")
+    }
+    server.start()
+
+    // Connect to the server
+    println("Connecting to server.")
+    Socket("localhost", 3000)
+    println("Connected to server.")
+    server.join()
 }
--- making/src/test/kotlin/org/geepawhill/rw/making/NothingTest.kt
+++ making/src/test/kotlin/org/geepawhill/rw/making/NothingTest.kt
@@ -1,13 +1,26 @@
 package org.geepawhill.rw.making
 
-import assertk.assertThat
-import assertk.assertions.*
 import org.junit.jupiter.api.Test
+import java.net.ServerSocket
+import java.net.Socket
 
 class NothingTest {
 
     @Test
     fun `Idunno2`() {
-        assertThat(true).isTrue()
+        // Start a server
+        val server = Thread {
+            println("Starting server thread.")
+            val socket = ServerSocket(3000)
+            socket.accept()
+            println("Ending server thread.")
+        }
+        server.start()
+
+        // Connect to the server
+        println("Connecting to server.")
+        Socket("localhost", 3000)
+        println("Connected to server.")
+        server.join()
     }
 }
