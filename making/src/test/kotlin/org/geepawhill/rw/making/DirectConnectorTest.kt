1c2a665768a939a019321191ea2e053b66584448
GeePawHill <geepawhill@geepawhill.org>
2022-03-30 10:52:09 -0400

    DirectConnector: connect returns true.

--- making/src/test/kotlin/org/geepawhill/rw/making/DirectConnectorTest.kt
+++ making/src/test/kotlin/org/geepawhill/rw/making/DirectConnectorTest.kt
@@ -2,18 +2,29 @@ package org.geepawhill.rw.making
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import assertk.assertions.isTrue
 import org.junit.jupiter.api.Test
 
 val DISCONNECTED_RESPONSE = "ERROR: Not Connected."
 
 class DirectConnectorTest {
+    val connector = DirectConnector()
 
     @Test
     fun `send before connect gets error response`() {
-        assertThat(DirectConnector().send("Hi Mom!")).isEqualTo(DISCONNECTED_RESPONSE)
+        assertThat(connector.send("Hi Mom!")).isEqualTo(DISCONNECTED_RESPONSE)
+    }
+
+    @Test
+    fun `connect returns true on success`() {
+        assertThat(connector.connect()).isTrue()
     }
 
     class DirectConnector {
+        fun connect(): Boolean {
+            return true
+        }
+
         fun send(message: String): String {
             return DISCONNECTED_RESPONSE
         }
