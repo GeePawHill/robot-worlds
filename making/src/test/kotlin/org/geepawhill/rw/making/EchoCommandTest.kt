f38bf6517894da46f5473c8b34ddb6c8f600a31e
GeePaw Hill <geepawhill@geepawhill.org>
2022-04-17 00:14:16 -0400

    Construct EchoCommand from well-formed Request

--- making/src/test/kotlin/org/geepawhill/rw/making/EchoCommandTest.kt
+++ making/src/test/kotlin/org/geepawhill/rw/making/EchoCommandTest.kt
@@ -5,6 +5,9 @@ import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
 
 class EchoCommand(val text: String) {
+
+    constructor(request: Request) : this(request.arguments[0].content)
+
     fun execute(): String {
         return Response.echoResponse(text).toJson()
     }
@@ -15,6 +18,12 @@ class EchoCommandTest {
     @Test
     fun `command returns echoResponse text`() {
         val command = EchoCommand("Hi Mom!")
+        assertThat(command.execute()).isEqualTo(Response.echoResponse("Hi Mom!").toJson())
+    }
+
+    @Test
+    fun `constructs from well-formed echo request, too`() {
+        val command = EchoCommand(Request.echoRequest("Hi Mom!"))
         assertThat(command.execute()).isEqualTo(Response.echoResponse("Hi Mom!").toJson())
     }
 }
