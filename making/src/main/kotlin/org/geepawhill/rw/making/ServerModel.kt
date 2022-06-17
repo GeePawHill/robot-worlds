e573e87d0a9f02f434489f8f1fd0d99e09c270d6
GeePaw Hill <geepawhill@geepawhill.org>
2022-06-17 10:29:53 -0400

    Make ServerModel a Receiver, too.

--- making/src/main/kotlin/org/geepawhill/rw/making/ServerModel.kt
+++ making/src/main/kotlin/org/geepawhill/rw/making/ServerModel.kt
@@ -1,12 +1,17 @@
 package org.geepawhill.rw.making
 
 import org.geepawhill.rw.command.Event
+import org.geepawhill.rw.transport.Receiver
 import tornadofx.*
 
-class ServerModel(val commander: Commander) : Watcher {
+class ServerModel(val commander: Commander) : Watcher, Receiver {
     val activity = observableListOf<Event>()
-    
+
     override fun notify(event: Event) {
         activity += event
+    }
+
+    override fun receive(message: String): String {
+        return commander.receive(message)
     }
 }
--- making/src/test/kotlin/org/geepawhill/rw/making/ServerModelTest.kt
+++ making/src/test/kotlin/org/geepawhill/rw/making/ServerModelTest.kt
@@ -2,7 +2,9 @@ package org.geepawhill.rw.making
 
 import assertk.assertThat
 import assertk.assertions.containsExactly
+import assertk.assertions.isEqualTo
 import org.geepawhill.rw.transport.Request
+import org.geepawhill.rw.transport.Response
 import org.junit.jupiter.api.Test
 
 class ServerModelTest {
@@ -15,5 +17,12 @@ class ServerModelTest {
         val event = Request.echoRequest("Hi Mom!")
         model.notify(event)
         assertThat(model.activity).containsExactly(event)
+    }
+
+    @Test
+    fun `receive passes on to Commander`() {
+        val event = Request.echoRequest("Hi Mom!")
+        val response = model.receive(event.toJson())
+        assertThat(response).isEqualTo(Response.echo("Hi Mom!").toJson())
     }
 }
