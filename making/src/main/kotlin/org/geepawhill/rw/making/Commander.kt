a4e9da703a560289c36f0bc36067f856200a9a03
GeePaw Hill <geepawhill@geepawhill.org>
2022-06-17 09:50:01 -0400

    Give Commander a Watcher for notifications.

--- making/src/main/kotlin/org/geepawhill/rw/making/Commander.kt
+++ making/src/main/kotlin/org/geepawhill/rw/making/Commander.kt
@@ -4,10 +4,11 @@ import org.geepawhill.rw.command.Command
 import org.geepawhill.rw.transport.Receiver
 import org.geepawhill.rw.transport.Request
 
-class Commander : Receiver {
+class Commander(val watcher: Watcher = Watcher.NONE) : Receiver {
 
     override fun receive(message: String): String {
         val request = Request.fromJson(message)
+        watcher.notify(request)
         val command = Command.fromRequest(request)
         return CommandRunner(command).run()
     }
--- making/src/test/kotlin/org/geepawhill/rw/making/CommanderTest.kt
+++ making/src/test/kotlin/org/geepawhill/rw/making/CommanderTest.kt
@@ -1,20 +1,31 @@
 package org.geepawhill.rw.making
 
 import assertk.assertThat
+import assertk.assertions.containsExactly
 import assertk.assertions.isEqualTo
+import org.geepawhill.rw.command.Event
 import org.geepawhill.rw.transport.Request
 import org.geepawhill.rw.transport.Response
 import org.junit.jupiter.api.Test
 
-class CommanderTest {
+class CommanderTest : Watcher {
 
-    val commander = Commander()
+    val events = mutableListOf<Event>()
+    val commander = Commander(this)
 
     @Test
     fun `echo request gets echo response`() {
         val response = commander.receive(Request.echoRequest("Hi Mom!").toJson())
         assertThat(response).isEqualTo(Response.echo("Hi Mom!").toJson())
     }
+
+    @Test
+    fun `echo request notifies watcher`() {
+        val request = Request.echoRequest("Hi Mom!")
+        val response = commander.receive(request.toJson())
+        assertThat(events).containsExactly(request)
+    }
+
 
     @Test
     fun `non-request gets UnparseableRequest`() {
@@ -32,5 +43,9 @@ class CommanderTest {
     fun `unparseable command bad or missing arguments gets UnparseableCommand`() {
         val response = commander.receive(Request("echo", "n/a", emptyList()).toJson())
         assertThat(response).isEqualTo(Response.unparseableCommand().toJson())
+    }
+
+    override fun notify(event: Event) {
+        events += event
     }
 }
